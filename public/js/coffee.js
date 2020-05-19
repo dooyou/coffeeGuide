dc309d0a8b511a14ee87878922c11c4bc4b47ab6
dooyou <adidas0331@naver.com>
2020-05-19 20:32:46 +0900

    switch문을 활용한 coffee guide

---
+++ public/js/coffee.js
@@ -0,0 +1,61 @@
+()=>{
+
+}
+
+  function whatcoffee(value) {
+
+    function coffeeOrder({ coffee }){
+      switch (coffee) {
+        case 'espresso':
+          return ['Espresso'];
+          break;
+        case 'americano':
+          return ['Espresso', 'Hot Water'];
+          break;
+        case 'latte':
+          return ['Espresso', 'Milk', 'Milk foam'];
+          break;
+        case 'cappuchino':
+          return ['Espresso', 'Milk', 'Cream'];
+          break;
+        case 'caramelmacchiato':
+          return ['Espresso', 'Caramel', 'Milk', 'Milk foam'];
+          break;
+        case 'amaretto':
+          return ['Espresso', 'Amaretto', 'Cream'];
+          break;
+        case 'conpanna':
+          return ['Espresso', 'Cream'];
+          break;
+        case 'flatwhite':
+          return ['Espresso', 'Milk'];
+          break;
+        case 'hawaiian':
+          return ['Espresso', 'Coconut Milk'];
+          break;
+        case 'irish':
+          return ['Espresso', 'Whisky', 'Cream'];
+          break;
+        case 'loungo':
+          return ['Espresso'];
+          break;
+        case 'macchiato':
+          return ['Espresso', 'Milk foam'];
+          break;
+        case 'milkpunch':
+          return ['Milk', 'Honey', 'Whisky'];
+          break;
+        case 'short':
+          return ['Espresso', 'Milk', 'Cream'];
+          break;
+
+
+
+        default:
+          return 'What do you want ?';
+      }//END switch
+    }//END coffeeOrder
+
+
+  document.getElementById('like').innerHTML = coffeeOrder({coffee:value});
+  }//END whatcoffee
